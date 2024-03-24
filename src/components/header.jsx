04919204db657c973f12b90de45b547fe9b3a9e8
saurav-ghimire <sauravghimire0123@gmail.com>
2024-03-24 18:12:06 -0400

    Some Montion Animation for Navbar and Logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -54,19 +54,27 @@ function Header() {
     <header className='w-screen fixed z-50 p-6 px-16'>
       {/* Desktop and Tablet */}
       <div className='hidden md:flex w-full h-full items-center justify-between'>
-        <div className='flex items-center gap-2'>
+        <motion.div
+        initial={{opacity:0, x:-200}}
+        animate={{opacity:1, x:0}}
+        exit={{opacity:0, x:-200}}
+         className='flex items-center gap-2'>
           <Link to="/">
             <img src={logo} className='w-8 object-cover' alt="" />
             <p className='text-headingColor text-xl font-bold'>City</p>
           </Link>
-        </div>
+        </motion.div>
         <div className='flex items-center justify-between'>
-          <ul className='flex items-center gap-8 '>
+          <motion.ul
+          initial = {{opacity : 0, x:200}}
+          animate = {{opacity : 1, x:0}}
+          exit = {{opacity : 0, x:200}}
+           className='flex items-center gap-8 '>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About us</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Services</li>
-          </ul>
+          </motion.ul>
           <div className="relative flex items-center justify-center">
             <IoBasket className='text-textColor text-2xl ml-8 cursor-pointer' />
             <div className="w-5 h-5 rounded-full bg-cartNumBg flex item-center justify-center absolute -top-1 -right-2">
@@ -83,7 +91,11 @@ function Header() {
               className='w-10 min-w-[40px] min-h-[40px] cursor-pointer rounded-3xl drop-shadow-2xl ml-8'
             />
             {toggleLogin && (
-              <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 overflow-hidden'>
+              <motion.div
+              initial = {{opacity : 0, scale:0.6}}
+              animate = {{opacity : 1, scale:1}}
+              exit = {{opacity : 0, scale:0.6}}
+              className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 overflow-hidden'>
                 {
                   userDetails && userDetails.email === "sauravghimire0123@gmail.com" && (
                     <p className='flex px-4 py-2 items-center cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base'>
@@ -101,7 +113,7 @@ function Header() {
                     </p>
                   )
                 }
-              </div>
+              </motion.div>
             )}
           </div>
         </div>
